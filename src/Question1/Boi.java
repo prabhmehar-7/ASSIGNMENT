c5da9874e0c30977e07d8a7577f52e17654183cc
prabhmehar-7 <prabhmehar.chhabra@tothenew.com>
2023-03-14 00:42:19 +0530

    java-2 Update

---
+++ src/Question1/Boi.java
@@ -0,0 +1,10 @@
+package Question1;
+
+class Boi extends Bank {
+    public Boi(String name, String headOfficeAddress, String chairmanName, int branchCount,
+               double fdInterestRate, double personalLoanInterestRate, double homeLoanInterestRate) {
+        super(name, headOfficeAddress, chairmanName, branchCount, fdInterestRate, personalLoanInterestRate, homeLoanInterestRate);
+
+    }
+
+}
---
+++ src/Question1/Icici.java
@@ -0,0 +1,9 @@
+package Question1;
+
+class Icici extends Bank {
+    public Icici(String name, String headOfficeAddress, String chairmanName, int branchCount,
+                 double fdInterestRate, double personalLoanInterestRate, double homeLoanInterestRate) {
+        super(name, headOfficeAddress, chairmanName, branchCount, fdInterestRate, personalLoanInterestRate, homeLoanInterestRate);
+    }
+
+}
